1b401bf95a7f3157d7da2964699c1e99bd730978
yutingh <yutingh@cw.com.tw>
2020-10-26 13:32:22 +0800

    smooth-scroll

--- assets/js/style.js
+++ assets/js/style.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
 			if (target.length) {
 				$("html, body").animate({
-					scrollTop: target.offset().top - headerHeight
+					scrollTop: target.offset().top
 				}, 1000, "easeInOutExpo");
 				return false;
 			}
