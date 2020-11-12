682e4e3abc8c34902a0153c41772f5c0f5212d83
yutingh <yutingh@cw.com.tw>
2020-11-12 13:34:42 +0800

    未來建築

--- assets/js/keywords.js
+++ assets/js/keywords.js
@@ -1,19 +1,19 @@
 var keys = [
     {
+        "keys": "未來建築",
+        "img": "future-city.png",
+        "hashtag": ["環境", "永續", "本住建築"],
+        "apple": "https://podcasts.apple.com/tw/podcast/2021%E9%97%9C%E9%8D%B5%E5%AD%97ep-2-%E6%9C%AA%E4%BE%86%E5%BB%BA%E7%AF%89-%E5%8D%8A%E8%B9%B2-%E9%98%B2%E7%96%AB-%E6%9C%83%E5%91%BC%E5%90%B8-%E8%83%BD%E8%88%87%E4%BA%BA%E6%BA%9D%E9%80%9A%E7%9A%84%E6%AD%A3%E8%83%BD%E9%87%8F%E5%BB%BA%E7%AF%89/id1486227803?i=1000498102490",
+        "google": "https://podcasts.google.com/?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9mYzk0ZmM4L3BvZGNhc3QvcnNz&ep=14&episode=MmM1MTNlMWEtM2RjYi00ZTY3LTg2OWItNDYzOWEzMmFhMjFl",
+        "article": "https://www.cw.com.tw/article/5102512"
+    },
+    {
         "keys": "2021關鍵字",
         "img": "us-election.jpeg",
         "hashtag": ["強人政治", "美國大選", "永續"],
         "apple": "https://podcasts.apple.com/tw/podcast/%E8%81%BD%E5%A4%A9%E4%B8%8B-%E5%A4%A9%E4%B8%8B%E9%9B%9C%E8%AA%8Cpodcast/id1486227803?i=1000497166002",
         "google": "https://podcasts.google.com/?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9mYzk0ZmM4L3BvZGNhc3QvcnNz&ep=14&episode=ODIzNzdmODItZjJlOS00ZTBlLWI5ZjgtMzc4NzlkYTdmZDRh",
         "article": "https://web.cw.com.tw/us-2020/"
-    },
-    {
-        "keys": "未來建築",
-        "img": "future-city.png",
-        "hashtag": ["環境", "永續", "本住建築"],
-        "apple": "https://podcasts.apple.com/tw/podcast/%E8%81%BD%E5%A4%A9%E4%B8%8B-%E5%A4%A9%E4%B8%8B%E9%9B%9C%E8%AA%8Cpodcast/id1486227803#episodeGuid=2c513e1a-3dcb-4e67-869b-4639a32aa21e",
-        "google": "https://podcasts.google.com/?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9mYzk0ZmM4L3BvZGNhc3QvcnNz&ep=14&episode=MmM1MTNlMWEtM2RjYi00ZTY3LTg2OWItNDYzOWEzMmFhMjFl",
-        "article": "https://www.cw.com.tw/article/5102512"
     },
     {
         "keys": "智慧聚落",
