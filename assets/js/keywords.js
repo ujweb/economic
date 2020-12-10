5849eec4b90e5de7b68c778e726ae4941b721246
yutingh <yutingh@cw.com.tw>
2020-12-10 15:11:59 +0800

    低利對策

--- assets/js/keywords.js
+++ assets/js/keywords.js
@@ -1,5 +1,13 @@
 var keys = [
     {
+        "keys": "低利對策",
+        "img": "cheap-money.jpg",
+        "hashtag": ["投資", "成長策略", "野村投信"],
+        "apple": "https://reurl.cc/q8ey8n",
+        "google": "https://reurl.cc/r8Wp8O",
+        "article": "https://www.cw.com.tw/article/5103035"
+    },
+    {
         "keys": "智慧急診",
         "img": "smart-emergency.jpg",
         "hashtag": ["智慧醫療", "急診室", "臺大人工智慧中心"],
@@ -38,14 +46,6 @@ var keys = [
         "apple": "https://podcasts.apple.com/tw/podcast/%E8%81%BD%E5%A4%A9%E4%B8%8B-%E5%A4%A9%E4%B8%8B%E9%9B%9C%E8%AA%8Cpodcast/id1486227803?i=1000497166002",
         "google": "https://podcasts.google.com/?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9mYzk0ZmM4L3BvZGNhc3QvcnNz&ep=14&episode=ODIzNzdmODItZjJlOS00ZTBlLWI5ZjgtMzc4NzlkYTdmZDRh",
         "article": "https://web.cw.com.tw/us-2020/"
-    },
-    {
-        "keys": "低利對策",
-        "img": "",
-        "hashtag": "",
-        "apple": "",
-        "google": "",
-        "article": ""
     },
     {
         "keys": "疫後K經濟",
@@ -118,7 +118,7 @@ for (var k = 0; k < keys.length; k++) {
         imgCard += '" target="_blank" class="btn btn--white h6 mt10 mb0">相關報導</a></div>';
     } else {
         imgCard += '<div class="img-card text-center"><h3 class="my0">' + keys[k]["keys"] + '</h3>';
-        imgCard += '<div class="h5 mt10 mb0">每週三更新<br/>敬請期待</div></div>';
+        imgCard += '<div class="h5 mt10 mb0">每週四更新<br/>敬請期待</div></div>';
     }
 }
 $('.img-card-list').html(imgCard);
