dfbf420942302f889ddecefc5465e3189e6feafe
yutingh <yutingh@cw.com.tw>
2021-01-05 14:15:13 +0800

    關鍵趨勢報導

--- assets/js/style.js
+++ assets/js/style.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
 			$(this).addClass('option-list');
 		}
 	})
-	$('.card-group').slick({
+	$('.card-group-slide').slick({
 		slidesToShow: 4,
 		arrows: true,
 		prevArrow: '<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M238.475 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L50.053 256 245.546 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L10.454 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z"></path></svg></div>',
@@ -62,6 +62,17 @@ $(document).ready(function () {
 			}
 		}],
 	});
+	function slideMobile() {
+		$('.slide-mb').slick({
+			slidesToShow: 1,
+			arrows: true,
+			prevArrow: '<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M238.475 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L50.053 256 245.546 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L10.454 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z"></path></svg></div>',
+			nextArrow: '<div class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M17.525 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L205.947 256 10.454 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L34.495 36.465c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg></div>',
+		});
+	}
+	if ( width <= 768 ) {
+		slideMobile();
+	}
 	function shuffle(arra1) {
 		var ctr = arra1.length, temp, index;
 		while (ctr > 0) {
@@ -91,6 +102,14 @@ $(document).ready(function () {
 	setInterval(function(){
 		random();
 	}, 10000);
+	$(window).on("load", function(e){
+		var width = $(window).width();
+		if ( width <= 768 ) {
+			slideMobile();
+		} else {
+			$('.slide-mb').slick('unslick');
+		}
+	});	
 	$(window).on("scroll", function () {
 		var scroll = $(window).scrollTop();
 		if (scroll >= height / 3) {
